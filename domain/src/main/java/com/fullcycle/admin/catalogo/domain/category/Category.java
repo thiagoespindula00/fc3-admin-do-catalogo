3ba359a38a5d452847e97889d58476d0189aea84
thiagoespindula00 <thiagoespindula00@gmail.com>
2025-04-21 20:59:50 -0300

    Criando a Categoria com TDD

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -0,0 +1,66 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class Category {
+    private String id;
+    private String name;
+    private String description;
+    private boolean isActive;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    private Category(
+            final String id,
+            final String name,
+            final String description,
+            final boolean isActive,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static Category newCategory(final String name, final String description, final boolean isActive) {
+        final var id = UUID.randomUUID().toString();
+        final var now = Instant.now();
+        return new Category(id, name, description, isActive, now, now, null);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+}
